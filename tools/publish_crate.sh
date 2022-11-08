c19e5a03a72eecb48160bda1eb94990fb234b61c
arkbig <35622611+arkbig@users.noreply.github.com>
2022-11-09 00:26:46 +0900

    👷Add publish action

---
+++ tools/publish_crate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# set -eux
+set -eu
+
+ROOTDIR=$(cd "$(dirname "$0")"/.. && pwd)
+
+crate_version=$(cd "$ROOTDIR" && cargo read-manifest | jq -r ".version")
+
+already_exists_tag=$(gh api repos/arkbig/throbber-widgets-tui/tags | jq "any(.[]; .name==\"v$crate_version\")")
+if $already_exists_tag; then
+    echo "Already exists tag v$crate_version"
+    exit
+fi
+
+git tag "v$crate_version"
+git push origin "v$crate_version"
+
+(cd "$ROOTDIR" && (echo \$CRATE_IO_TOKEN | envsubst | cargo login) && cargo publish)
